5dde1cbdfb30afc6f9935776fc6b8ac0094df952
Amulet9 <someone@gmail.com>
2024-11-17 18:31:11 +0530

    improved constants.json and constants.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from helpers.runner import runner
 from pages.main_menu import main_menu
 from pages.loading_screen import loading_screen
 from pages.main_game import main_game
-
+from helpers.constants import track
 
 chdir(dirname(__file__))
 
@@ -15,7 +15,7 @@ init()
 display.set_caption("BrickMania")
 
 if __name__ == "__main__":
-    music.load("./assets/music.mp3")
+    music = track
     is_paused=True
     type_ = 1
     runner(main_menu, loading_screen, main_game, type_, is_paused)
