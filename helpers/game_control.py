846fdafa1aab5e40ca1216aff71bae441370fade
Neeladri Mullick <neeladrijlt@gmail.com>
2024-11-17 09:33:46 +0530

    Variable CHanges

--- helpers/game_control.py
+++ helpers/game_control.py
@@ -1,33 +1,33 @@
-import pygame
 import helpers.settings as settings
 from helpers.constants import track3, screen, SCALE, WIDTH, HEIGHT, clock, brick_cols, brick_height, brick_width, brick_rows
 import time
 from models import Color, PowerUp, Brick
 from random import random, choice
-from pygame import QUIT, quit, K_q, K_RETURN, KEYDOWN, event, font, mixer
+from pygame import  Surface, SRCALPHA, QUIT, quit, K_q, K_RETURN, K_p, K_SPACE, KEYDOWN, event, font
+from pygame.mixer import music
 from pygame.display import flip
 import sys
 
 def pause_game(font):
     paused = True
     pause_text = font.render("Game Paused. Press 'P' to Resume.", True, Color.WHITE)
-    dim_surface = pygame.Surface((WIDTH, HEIGHT), pygame.SRCALPHA)
+    dim_surface = Surface((WIDTH, HEIGHT), SRCALPHA)
     dim_surface.fill((0, 0, 0, 180))
     while paused:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
+        for e in event.get():
+            if e.type == QUIT:
+                quit()
                 sys.exit()
-            if event.type == pygame.KEYDOWN:
-                if event.key in (pygame.K_p, pygame.K_SPACE, pygame.K_0):
+            if e.type == KEYDOWN:
+                if event.key in (K_p, K_SPACE):
                     paused = False
-                if event.key == pygame.K_q:
-                    pygame.quit()
+                if e.key == K_q:
+                    quit()
                     sys.exit()                    
         
         screen.blit(dim_surface, (0, 0))
         screen.blit(pause_text, ((WIDTH - pause_text.get_width()) // 2, HEIGHT // 2))
-        pygame.display.flip()
+        flip()
 
         clock.tick(10)
 
@@ -35,11 +35,11 @@ def pause_game(font):
 
 def game_over(score, settings: settings.Settings, type_=0, mode=0):
     if not mode:
-        pygame.mixer.music.pause()
+        music.pause()
         track3.play()
         curr = time.time()
     screen.fill(Color.BLACK)
-    font_for_game_over = pygame.font.SysFont(None, int(42 * SCALE))
+    font_for_game_over = font.SysFont(None, int(42 * SCALE))
     text = font_for_game_over.render("Game Over! Press ENTER to restart", True, Color.RED)
     highscore = settings.highscore
     text2 = font_for_game_over.render(f"High Score = {[score, highscore][highscore>score]}", True, [Color.GREEN, Color.YELLOW][highscore>score])
@@ -50,21 +50,21 @@ def game_over(score, settings: settings.Settings, type_=0, mode=0):
     screen.blit(text, ((WIDTH // 2 - text.get_width() / 2) * SCALE, (HEIGHT // 2 - 40) * SCALE))
     screen.blit(text2, ((WIDTH // 2 - text2.get_width() / 2) * SCALE, (HEIGHT // 2) * SCALE))
     screen.blit(text3, ((WIDTH // 2 - text3.get_width() / 2) * SCALE, (HEIGHT // 2 + 40) * SCALE))
-    pygame.display.flip()
+    flip()
 
     while True:
         if not mode and time.time() - curr > 2:
             if not mode:
                 track3.stop()
-                pygame.mixer.music.unpause()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
+                music.unpause()
+        for e in event.get():
+            if e.type == QUIT:
+                quit()
                 sys.exit()
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+            if e.type == KEYDOWN and e.key == K_RETURN:
                 return
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_q:
-                pygame.quit()
+            if e.type == KEYDOWN and e.key == K_q:
+                quit()
                 sys.exit()
 
 
