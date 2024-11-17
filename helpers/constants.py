44b3d868fcef8a0e919f3b24fe66a6d338013e67
Amulet9 <someone@gmail.com>
2024-11-17 17:05:09 +0530

    improved constants.json

--- helpers/constants.py
+++ helpers/constants.py
@@ -1,5 +1,111 @@
 import pygame
 
+from pydantic import BaseModel
+from typing import List, Optional
+import os
+
+
+class Screen(BaseModel):
+    scale: float
+    width: int
+    height: int
+
+
+class PlayerInactivity(BaseModel):
+    multiplier: int
+    threshold: int
+
+
+class Player(BaseModel):
+    width: int
+    height: int
+    speed: int
+    inactivity: PlayerInactivity
+
+
+class Ball(BaseModel):
+    radius: int
+    speed_x: int
+    speed_y: int
+    speed_increment: float
+
+
+class Brick(BaseModel):
+    width: int
+    height: int
+    speed: int
+    rows: int
+    move_speed: int
+
+
+class PowerUp(BaseModel):
+    width: int
+    height: int
+    fall_speed: int
+    probability: float
+    max_length: int
+
+
+class FallingTile(BaseModel):
+    x_min: int
+    y_max: int
+    speed_upper: int
+    speed_lower: int
+    count: int
+
+
+class Interval(BaseModel):
+    special_ball: int
+    random_destruction: int
+
+
+class Duration(BaseModel):
+    special_ball: int
+    random_destruction: int
+
+
+class FontSize(BaseModel):
+    main: int
+    game_over: int
+
+    class MainMenu(BaseModel):
+        title: int
+        menu: int
+
+    main_menu: MainMenu
+
+
+class Tracks(BaseModel):
+    path: str
+    files: List[str]
+    extension: str
+
+
+class GameConstants(BaseModel):
+    title: str
+    inactivity_threshold: int
+    interval: Interval
+    duration: Duration
+    font_size: FontSize
+    game_modes: List[str]
+    frames_per_second: int
+    tracks: Tracks
+
+
+class Config(BaseModel):
+    screen: Screen
+    player: Player
+    ball: Ball
+    brick: Brick
+    powerup: PowerUp
+    falling_tile: FallingTile
+    game_constants: GameConstants
+
+    @classmethod
+    def open(cls) -> Optional['Config']:
+        cls.model_validate_json(open(os.getenv("BRICKMANIA_CONFIGURATION", "./constants.json"), 'r').read())
+
+
 pygame.init()
 SCALE = 1
 WIDTH, HEIGHT = int(800 * SCALE), int(600 * SCALE)
@@ -29,5 +135,3 @@ speed_increment = 1.0001
 track1 = pygame.mixer.Sound("./assets/music1.mp3")
 track2 = pygame.mixer.Sound("./assets/music2.mp3")
 track3 = pygame.mixer.Sound("./assets/music3.mp3")
-
-
