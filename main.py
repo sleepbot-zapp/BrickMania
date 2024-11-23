8cded20fc624ea8236c1ab1095aec4f4268f292f
Neeladri Mullick <neeladrijlt@gmail.com>
2024-11-23 23:25:06 +0530

    remove delays

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ from pages import Info, MainGame, MainMenu, Settings
 from pages import loading_screen
 
 
+import pygame
+import sys
+
 class Game:
     def __init__(
         self,
@@ -41,7 +44,19 @@ class Game:
         self.volume = 0.0
         
         self.trails = {}
-        
+
+        # Initialize page objects lazily
+        self.main_menu = None
+        self.settings_page = None
+        self.info_page = None
+        self.game_page = None
+
+    def pre_load_music(self):
+        # Pre-load music files and set their volume
+        for music_file in self._music_files[1:]:
+            music_file.set_volume(self.volume)
+
+    def initialize_pages(self):
         self.main_menu = MainMenu(self.screen, self.height, self.width, self.scale, self)
         self.settings_page = Settings(
             self.screen,
@@ -59,20 +74,15 @@ class Game:
 
     @property
     def music_files(self):
-        music_files = []
-        for music_file in self._music_files[1:]:
-            music_files.append(music_file)
-            music_file.set_volume(self.volume)
-        return music_files
+        return self._music_files[1:]  # Avoid modifying the original list
 
     def run_loading_screen(self):
         """Call the dynamic loading screen function."""
-        result = (
-            loading_screen(self.colors)
-        )  
-        return result  
+        return loading_screen(self.colors)  # Simplified call
 
     def gameloop(self):
+        self.pre_load_music()  # Pre-load music to avoid delay in the loop
+
         while True:
             if self.music_is_playing:
                 pygame.mixer.music.load(self._music_files[0])
@@ -82,8 +92,9 @@ class Game:
 
             self.screen.fill(self.colors.BLACK)
 
-            
             if self.is_main_menu:
+                if not self.main_menu:
+                    self.initialize_pages()  # Initialize pages when needed
                 selected_option = self.main_menu.generate(
                     self.colors, brick_width, brick_height, self.clock
                 )
@@ -93,26 +104,19 @@ class Game:
                     pygame.quit()
                     sys.exit()
 
-            
-            if selected_option == 0:  
+            if selected_option == 0:
                 self.is_main_menu = False
-                back_to_main_menu = (
-                    self.run_loading_screen()
-                )  
-                if not back_to_main_menu:  
+                back_to_main_menu = self.run_loading_screen()
+                if not back_to_main_menu:
                     self.is_main_menu = self.game_page.runner(
-                        self.colors,
-                        brick_height,
-                        brick_width,
-                        self.trails,
-                        self.clock,
+                        self.colors, brick_height, brick_width, self.trails, self.clock
                     )
-                else:  
+                else:
                     self.is_main_menu = True
-            elif selected_option == 1:  
+            elif selected_option == 1:
                 self.is_main_menu = False
                 self.is_main_menu = self.settings_page.display()
-            elif selected_option == 2:  
+            elif selected_option == 2:
                 self.is_main_menu = False
                 self.is_main_menu = self.info_page.scroll(self.colors)
 
